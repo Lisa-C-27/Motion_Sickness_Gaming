310538e24467cfbfed03a14bc67c35ac8bf36d8f
Lisa Clark <sweet_kandi_27@hotmail.com>
2018-03-27 12:18:14 +1000

    Update registration_process.php

--- controller/registration_process.php
+++ controller/registration_process.php
@@ -1,17 +1,31 @@
 <?php
+    session_start();
 
     if( isset( $_POST['registration_form'] ) ) {
+        
+    $login_sql = "SELECT * FROM user WHERE username = '" . $_POST['username'] . "';";
+    include '../model/connect.php';
+    $stmt = $conn->prepare($login_sql);
+    $stmt->execute();
+    $result = $stmt->fetch();
 
-        $username = $_POST['user_name'];
-        $password = $_POST['user_pass'];
+        if ($stmt->rowCount() == 0){
 
-        $insertuser = "INSERT INTO user (username, password) VALUES( '$username','$password' )";
-        include '../model/connect.php';
-        $stmt = $conn->prepare($insertuser);
-        $stmt->execute();
+            $username = $_POST['username'];
+            $password = $_POST['userpass'];
 
-        echo "User created, please log in";  
-        header('location: ../view/php/registration_login.php');
-  
+            $insertuser = "INSERT INTO user (username, password) VALUES( '$username','$password' )";
+            include '../model/connect.php';
+            $stmt = $conn->prepare($insertuser);
+            $stmt->execute();
+            
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['userid'] = $result['user_ID'];
+            header('location: ../view/php/index.php');
+            } else {
+            $_SESSION['message'] = "Username already exists";   
+            header('Location: ../view/php/register.php');        
+            }     
     }
 ?>
