d04305a874c413b47695734303589ee0ba7f2c25
Lisa Clark <sweet_kandi_27@hotmail.com>
2018-03-27 12:16:48 +1000

    Update dbfunctions.php

--- model/dbfunctions.php
+++ model/dbfunctions.php
@@ -16,4 +16,27 @@ function get_one_game($gameID) {
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
+function updatethumbsup($gameid) {
+    $thumbsup = "UPDATE game SET gameThUp = gameThUp + 1 WHERE gameID ='" . $gameid . "'";
+    include 'connect.php';
+    $stmt = $conn->prepare($thumbsup);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function updatethumbsdown($gameid) {
+    $thumbsdown = "UPDATE game SET gameThDown = gameThDown + 1 WHERE gameID ='" . $gameid . "'";
+    include 'connect.php';
+    $stmt = $conn->prepare($thumbsdown);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 ?>
